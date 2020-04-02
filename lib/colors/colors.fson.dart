9c0587b06f4cabdc93c4cfea41e79fd0a2ec6a7f
Onur Özkan <onur-ozkan@hotmail.de>
2020-04-02 13:34:06 +0200

    clean up

--- lib/colors/colors.fson.dart
+++
@@ -1,7 +0,0 @@
-import 'package:string_res/string_res.dart';
-class RColors {
-	static RColor final_color = RColor(map: {"day": "FUCK YES"} ,name: "final_color");
-	static RColor color_two = RColor(map: {"day": "FUCK YES"} ,name: "color_two");
-	static RColor finalColor = RColor(map: {"day": "FUCK YES"} ,name: "finalColor");
-	static RColor myColor = RColor(map: {"day": "FUCK YES"} ,name: "myColor");
-}
